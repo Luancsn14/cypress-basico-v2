ec7d4f0544d0b618438ca7e89977ab4d301d0810
luan2 <luancsn14@gmail.com>
2022-12-08 08:37:34 -0300

    Segundo teste do projeto e gancho beforeEach.

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,24 @@
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    it('Verifica o título da aplicação', function() {
+describe('Central de Atendimento ao Cliente TAT', function () {
+    beforeEach(function () {
         cy.visit('./src/index.html')
-        cy.title().should('eq','Central de Atendimento ao Cliente TAT')
     })
+
+    it('Verifica o título da aplicação', function () {
+        cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('Preenche os campos obrigatórios e envia o formulário', function () {
+        cy.get('#firstName').type('Luan')
+        cy.get('#lastName').type('Castro')
+        cy.get('#email').type('luan@email.com')
+        cy.get('#phone').type('71999999999')
+        cy.get('#open-text-area').type('Teste')
+
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success > strong').should('be.visible')
+        
+   })
 })
